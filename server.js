bde426b1c37b495fda5101b4dddf351cb9388726
Shwayoe <tinmgmgyin@gmail.com>
2017-08-06 17:42:05 +0630

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -86,14 +86,6 @@ app.get('/:articleName', function(req,res){
     res.send(createTemplate(articles[articleName]));
 });
 
-app.get('/article_two', function(req,res){
-    res.sendFile(path.join(__dirname, 'ui', 'article_two.html'));
-});
-
-app.get('/article_three', function(req,res){
-    res.sendFile(path.join(__dirname, 'ui', 'article_three.html'));
-});
-
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });

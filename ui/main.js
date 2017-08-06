215196c84062eaf839b4d335cb534ee9f61790b4
Shwayoe <tinmgmgyin@gmail.com>
2017-08-06 18:11:38 +0630

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,1 +1,12 @@
-console.log('Loaded!');
+var button = document.getElementById('counter');
+var counter = 0;
+button.onclick = function(){
+    // Make request
+    
+    //Capture the  response
+    
+    //Render the data.
+    counter = counter +1;
+    var span = document.getElementById('count');
+    span.innerHTML = counter.toString();
+}

a0ffe298058a6cb0f9ee260ee1323b006b080373
Shwayoe <tinmgmgyin@gmail.com>
2017-08-06 16:15:36 +0630

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,6 +9,18 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
+app.get('/article_one', function(req,res){
+    res.send('Article one has been requested and here is result.');    
+});
+
+app.get('/article_two', function(req,res){
+    res.send('Article two has been requested and here is result.');    
+});
+
+app.get('/article_three', function(req,res){
+    res.send('Article three has been requested and here is result.');    
+});
+
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });

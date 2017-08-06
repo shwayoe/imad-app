655433de939d565dcaacbb6a49572cce4a03e811
Shwayoe <tinmgmgyin@gmail.com>
2017-08-06 17:37:40 +0630

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
-var articles {
+var articles = {
     articleOne: {
       title: 'Article One | Shwayoe',
       heading: 'Article One',
@@ -19,6 +19,24 @@ var articles {
         </p>
         <p>
             It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layoutThe point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).
+        </p>`
+    },
+    articleTwo: {
+        title: 'Article Three | Shwayoe',
+      heading: 'Article Three',
+      date: 'Sep 17, 2016',
+      content: `
+        <p>
+            It is a long established fact that a reader will be distracted by the readable content of a page.
+        </p>`
+    },
+    articleThree: {
+        title: 'Article Three | Shwayoe',
+      heading: 'Article Three',
+      date: 'Sep 17, 2016',
+      content: `
+        <p>
+            It is a long established fact that a reader will be distracted by the readable content of a page.
         </p>`
     }
 };

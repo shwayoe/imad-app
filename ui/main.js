9cbb87e6e602d8567f53c148865ff61da1df5bb5
Shwayoe <tinmgmgyin@gmail.com>
2017-08-06 18:22:37 +0630

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,10 +1,10 @@
 var button = document.getElementById('counter');
-var counter = 0;
+
 button.onclick = function(){
     // create request
     var request = new XMLHttpRequest();
     
-    request.onstatechange = function(){
+    request.onreadystatechange = function(){
         if(request.readyState === XMLHttpRequest.DONE){
             
             if(request.status === 200){
@@ -17,5 +17,4 @@ button.onclick = function(){
     
     request.open('GET','http://tinmgmgyin.imad.hasura-app.io/counter',true);
     request.send(null);
-    
 };
